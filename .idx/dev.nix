6c8c1873983ac09936d8c9718c05c552c5f86958
danfiala23 <danfiala23@gmail.com>
2024-08-02 06:48:37 +0000

    feat(ide) added support for idx

---
+++ .idx/dev.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.jdk21
+    pkgs.maven
+  ];
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "redhat.java"
+    "vscjava.vscode-java-debug"
+    "vscjava.vscode-java-dependency"
+    "vscjava.vscode-java-pack"
+    "vscjava.vscode-java-test"
+    "vscjava.vscode-maven"
+  ];
+}
