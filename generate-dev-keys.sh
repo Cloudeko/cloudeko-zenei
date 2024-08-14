684bb559fc14b66368d1a2fce50b1bfbcc3b566f
Daniel Fiala <danfiala23@gmail.com>
2024-08-14 22:48:18 +0200

    fix(workflow): fixed the workflow key generation logic to use the script

--- generate-dev-keys.sh
+++ generate-dev-keys.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create a directory to store the keys
+mkdir -p core/src/main/resources
+
 # Generate a development private key
 openssl genpkey -algorithm RSA -out core/src/main/resources/dev-private-key.pem -pkeyopt rsa_keygen_bits:2048
 
